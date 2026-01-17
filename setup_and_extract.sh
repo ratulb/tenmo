cdb112483d1e4c5395cf22352042015660201428
Ratul <ratul75@hotmail.com>
2026-01-17 15:32:45 +0530

    Update pip upgrade comment in setup script
    Removed recommendation note for upgrading pip.

--- setup_and_extract.sh
+++ setup_and_extract.sh
@@ -10,7 +10,7 @@ python3 -m venv .venv
 # 2. Activate the virtual environment
 source .venv/bin/activate
 
-# 3. Upgrade pip (recommended)
+# 3. Upgrade pip 
 pip install --upgrade pip
 
 # 4. Install required Python package (TensorFlow)
