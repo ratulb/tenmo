1dec12c43afcf2aab286350934645a606132befc
Ratul Buragohain <ratul75@hotmail.com>
2026-03-26 21:52:41 +0000

    WIP

--- execute.sh
+++ execute.sh
@@ -3,7 +3,7 @@ clear
 # Check if an argument was provided
 if [ $# -eq 0 ]; then
     echo "Error: No test specified"
-    echo "Usage: $0 [minmax_scalar|contiguous|item|scalar|broadcast|gpu_expand|expand|gpusummean|gpu|npiop|sgd|matmul|cnn|chunk|fill|pad|logarithm|stack|concat|std_variance|blas|dropout|indexhelper|utils|variance|tanh|losses|data|intarray|tensors|mmnd|mm2d|mv|vm|argminmax|minmax|repeat|tiles|slice|linspace|softmax|relu|shuffle|buffers|flatten|permute|squeeze|unsqueeze|views|gradbox|ndb|transpose|shapes|strides|bench|validators|ce|synth_mnist|shapebroadcast|all]"
+    echo "Usage: $0 [maxmin_scalar|contiguous|item|scalar|broadcast|gpu_expand|expand|gpusummean|gpu|npiop|sgd|matmul|cnn|chunk|fill|pad|logarithm|stack|concat|std_variance|blas|dropout|indexhelper|utils|variance|tanh|losses|data|intarray|tensors|mmnd|mm2d|mv|vm|argminmax|minmax|repeat|tiles|slice|linspace|softmax|relu|shuffle|buffers|flatten|permute|squeeze|unsqueeze|views|gradbox|ndb|transpose|shapes|strides|bench|validators|ce|synth_mnist|shapebroadcast|all]"
     exit 1
 fi
 
@@ -30,7 +30,7 @@ case $1 in
         echo "Running mojo -I . tests/test_contiguous.mojo"
         mojo -I . $DEBUG_MODE tests/test_contiguous.mojo
         ;;
-    minmax_scalar)
+    maxmin_scalar)
         echo "Running mojo -I . tests/test_max_min_scalar.mojo"
         mojo -I . $DEBUG_MODE tests/test_max_min_scalar.mojo
         ;;
@@ -448,7 +448,7 @@ case $1 in
         ;;
     *)
         echo "Error: Unknown test '$1'"
-        echo "Available tests: minmax_scalar, contiguous, item, scalar, broadcast, gpu_expand, expand, gpusummean, gpu, npiop, sgd, matmul, cnn, chunk, fill, pad, logarithm, stack, concat, std_variance, dropout, blas, indexhelper, utils, variance, tanh, losses, data, intarray, mmnd, mm2d, vm, mv, repeat, tiles, linspace, slice, relu, softmax, permute, shuffle, argminmax, minmax, tensors, flatten, squeeze, unsqueeze, transpose, gradbox, ndb, buffers, views, shapes, strides, shapebroadcast, bench, validators, ce, synth_mnist, all"
+        echo "Available tests: maxmin_scalar, contiguous, item, scalar, broadcast, gpu_expand, expand, gpusummean, gpu, npiop, sgd, matmul, cnn, chunk, fill, pad, logarithm, stack, concat, std_variance, dropout, blas, indexhelper, utils, variance, tanh, losses, data, intarray, mmnd, mm2d, vm, mv, repeat, tiles, linspace, slice, relu, softmax, permute, shuffle, argminmax, minmax, tensors, flatten, squeeze, unsqueeze, transpose, gradbox, ndb, buffers, views, shapes, strides, shapebroadcast, bench, validators, ce, synth_mnist, all"
         exit 1
         ;;
 esac
