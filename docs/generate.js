0383964816028b710fac53bf8989867df107d6a0
Ratul Buragohain <ratul75@hotmail.com>
2026-04-25 05:27:53 +0000

    Fix md path

--- docs/generate.js
+++ docs/generate.js
@@ -2,7 +2,7 @@ const { marked } = require('marked');
 const fs = require('fs');
 const path = require('path');
 
-const docsDir = path.join(__dirname, 'docs', 'docs');
+const docsDir = path.join(__dirname, 'docs', 'md');
 const outDir = path.join(__dirname, 'build');
 
 // Convert /mojo/std/... to docs.modular.com/mojo/stdlib/...
